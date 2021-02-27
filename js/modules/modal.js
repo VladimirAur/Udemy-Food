ff3b08db5829435342f16543592dd377a9b6e4f4
Vladimir <vladimiraurelius@gmail.com>
2021-02-27 14:30:17 +0200

    modules_wp

---
+++ js/modules/modal.js
@@ -0,0 +1,57 @@
+function openModal(modalSelector, modalTimerId) {
+    const modal = document.querySelector(modalSelector);
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+    document.body.style.overflow = 'hidden';
+
+    if(modalTimerId){
+        clearInterval(modalTimerId);
+    }
+    
+}    
+
+function closeModal(modalSelector) {
+    const modal = document.querySelector(modalSelector);
+    modal.classList.remove('show');
+    modal.classList.add('hide');
+    document.body.style.overflow = '';
+} 
+
+function modal(triggerSelector, modalSelector, modalTimerId) {
+    const modalTrigger = document.querySelectorAll(triggerSelector),
+        modal = document.querySelector(modalSelector);
+        
+
+    function showByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight){
+            openModal(modalSelector, modalTimerId);
+            window.removeEventListener('scroll', showByScroll);
+        }
+    }
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', () => openModal(modalSelector, modalTimerId));
+    });    
+    
+    
+    
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal || e.target.getAttribute('data-close') == '') {
+            closeModal(modalSelector);
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === "Escape" && modal.classList.contains('show')) {
+            closeModal(modalSelector);
+        }
+    });
+
+    
+    
+    window.addEventListener('scroll', showByScroll);
+}
+
+export default modal;
+export {openModal};
+export {closeModal};
